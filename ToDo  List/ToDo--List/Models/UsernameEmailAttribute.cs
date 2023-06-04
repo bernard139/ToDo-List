0cf4ae20ceefc03149fae59dea39eea6f19f6179
bernard139 <aghedob36@gmail.com>
2023-06-04 11:39:11 +0100

    new

--- ToDo  List/ToDo--List/Models/UsernameEmailAttribute.cs
+++ ToDo  List/ToDo--List/Models/UsernameEmailAttribute.cs
@@ -1,37 +1,37 @@
-﻿//using System;
-//using System.ComponentModel.DataAnnotations;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
-//public class UsernameOrEmailAttribute : ValidationAttribute
-//{
-//    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-//    {
-//        var userManager = validationContext.GetService<UserManager<IdentityUser>>();
+public class UsernameOrEmailAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var userManager = validationContext.GetService<UserManager<IdentityUser>>();
 
-//        var usernameOrEmail = value?.ToString();
-//        if (!string.IsNullOrEmpty(usernameOrEmail))
-//        {
-//            var user = userManager.FindByEmailAsync(usernameOrEmail).Result
-//                       ?? userManager.FindByNameAsync(usernameOrEmail).Result;
+        var usernameOrEmail = value?.ToString();
+        if (!string.IsNullOrEmpty(usernameOrEmail))
+        {
+            var user = userManager.FindByEmailAsync(usernameOrEmail).Result
+                       ?? userManager.FindByNameAsync(usernameOrEmail).Result;
 
-//            if (user == null)
-//            {
-//                return new ValidationResult("Invalid username or email.");
-//            }
-//        }
-//        else
-//        {
-//            var property = validationContext.ObjectType.GetProperty(validationContext.MemberName);
-//            var requiredAttribute = property.GetCustomAttributes(typeof(RequiredAttribute), true)
-//                as RequiredAttribute[];
+            if (user == null)
+            {
+                return new ValidationResult("Invalid username or email.");
+            }
+        }
+        else
+        {
+            var property = validationContext.ObjectType.GetProperty(validationContext.MemberName);
+            var requiredAttribute = property.GetCustomAttributes(typeof(RequiredAttribute), true)
+                as RequiredAttribute[];
 
-//            if (requiredAttribute?.Length > 0)
-//            {
-//                return new ValidationResult(requiredAttribute[0].ErrorMessage);
-//            }
-//        }
+            if (requiredAttribute?.Length > 0)
+            {
+                return new ValidationResult(requiredAttribute[0].ErrorMessage);
+            }
+        }
 
-//        return ValidationResult.Success;
-//    }
-//}
+        return ValidationResult.Success;
+    }
+}
